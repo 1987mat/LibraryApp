10404d411ca281c8862991d51bf36b5d335d8de4
matcalvani <matstraty@gmail.com>
2022-09-29 13:09:11 -0700

    responsive

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // DOM elements
-const form = document.querySelector('.form');
-const addBtn = document.querySelector('#add-btn');
+const popUp = document.querySelector('.pop-up');
+const form = document.querySelector('form');
+const heading = document.querySelector('.heading');
 const btnNewBook = document.querySelector('#addBook-btn');
 const cancelBtn = document.querySelector('#cancel-btn');
 const list = document.querySelector('.list');
 const table = document.querySelector('.table-box');
 const addBookDiv = document.querySelector('.add-book-div');
+const clearBtn = document.querySelector('.clear-list-btn');
 
 // Empty array to store books
 let myLibrary = [];
@@ -20,24 +22,7 @@ class Book {
   }
 }
 
-// Load page event listener
-document.addEventListener('DOMContentLoaded', () => {
-  getLibrary();
-
-  let row;
-  for (let b of myLibrary) {
-    row = document.createElement('tr');
-    row.innerHTML = `<td>${b.title}</td>
-      <td>${b.author}</td>
-      <td>${b.pages}</td>
-      <td>${b.read}</td>
-      <td><button class="toggle">Change read status</button></td>
-      <td><a href="#" class="btn delete">X</a></td>`;
-    list.appendChild(row);
-  }
-});
-
-// Get books from array in Local Storage
+// Get books from Local Storage
 function getLibrary() {
   if (localStorage.getItem('myLibrary') === null) {
     myLibrary = [];
@@ -47,58 +32,124 @@ function getLibrary() {
   return myLibrary;
 }
 
-// Event listener for add book button
-btnNewBook.addEventListener('click', () => {
-  form.style.display = 'block';
-  table.style.display = 'none';
-  addBookDiv.style.display = 'none';
+document.addEventListener('DOMContentLoaded', () => {
+  getLibrary();
+
+  showHideTable();
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    let row = document.createElement('tr');
+    row.setAttribute('id', `${i}`);
+    row.classList.add('row');
+    row.innerHTML = `<td>${myLibrary[i].title}</td>
+      <td>${myLibrary[i].author}</td>
+      <td>${myLibrary[i].pages}</td>
+      <td>${myLibrary[i].read}</td>
+      <td><button class="toggle-status">Change read status</button></td>
+      <td><a href="#" class="btn-delete"><i class="fa fa-trash" aria-hidden="true"></i>`;
+    list.appendChild(row);
+  }
 });
 
-// Event listener submit form
+// Add new book
+btnNewBook.addEventListener('click', () => {
+  popUp.classList.add('show');
+  heading.classList.add('hide');
+  addBookDiv.classList.add('hide');
+  table.classList.remove('show');
+});
+
+function showHideTable() {
+  if (!myLibrary.length) {
+    table.classList.remove('show');
+    clearBtn.classList.remove('show');
+  } else {
+    table.classList.add('show');
+    clearBtn.classList.add('show');
+  }
+}
+
+cancelBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  showHideTable();
+  addBookDiv.classList.remove('hide');
+  heading.classList.remove('hide');
+  popUp.classList.remove('show');
+  form.reset();
+});
+
+// Submit form
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  // Hide form and show home page
-  table.style.display = 'block';
-  addBookDiv.style.display = 'block';
-  form.style.display = 'none';
+
+  // Hide popup and show home page
+  addBookDiv.classList.remove('hide');
+  heading.classList.remove('hide');
+  popUp.classList.remove('show');
 
   // Get user input values
   let title = document.querySelector('#title').value;
   let author = document.querySelector('#author').value;
   let pages = document.querySelector('#num-pages').value;
-  let read = getRead();
+  let read = getReadStatus();
 
   // Instantiate new book
   const book = new Book(author, title, pages, read);
 
-  // Push book to the library, show it on the UI and clear the form
+  // Push book to the library, show it on the UI
   myLibrary.push(book);
-
-  // Create table and display the book
-  const row = document.createElement('tr');
-  myLibrary.forEach((value) => {
-    // Add the book to the table
-    row.innerHTML = `
-      <td>${value.title}</td>
-      <td>${value.author}</td>
-      <td>${value.pages}</td>
-      <td id="yes-no-value">${value.read}</td>
-      <td><button class="toggle">Change read status</button></td>
-      <td><a href="#" class="btn delete">X</a></td>`;
-  });
-  list.appendChild(row);
+  renderBook();
 
   // Add book to Local Storage
   localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 
+  showHideTable();
   // Show success alert
-  showAlert('Book added!', 'success');
+  showAlert('Book added', 'success');
 
   // Clear form
   form.reset();
 });
 
-function getRead() {
+clearBtn.addEventListener('click', () => {
+  // Show popup
+  swal('Are you sure you want to clear the list?', {
+    buttons: ['Cancel', 'Yes'],
+  }).then((val) => {
+    if (val) {
+      swal({
+        title: 'Thanks!',
+        text: 'Your list is now empty',
+        icon: 'success',
+      });
+
+      myLibrary.splice(0);
+      localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+      table.classList.remove('show');
+      clearBtn.classList.remove('show');
+    }
+  });
+});
+
+function renderBook() {
+  let row = document.createElement('tr');
+
+  myLibrary.forEach((value, index) => {
+    row.setAttribute('id', `${index}`);
+    row.classList.add('row');
+    row.innerHTML = `
+      <td>${value.title}</td>
+      <td>${value.author}</td>
+      <td>${value.pages}</td>
+      <td id="yes-no-value">${value.read}</td>
+      <td><button class="toggle-status">Change read status</button></td>
+      <td><a href="#" class="btn-delete"><i class="fa fa-trash" aria-hidden="true"></i>
+      </a></td>`;
+    list.appendChild(row);
+  });
+}
+
+function getReadStatus() {
   const radioBtn = document.querySelectorAll('input[name="radio"]');
   let selectValue;
 
@@ -123,22 +174,16 @@ function showAlert(message, className) {
   setTimeout(() => document.querySelector('.alert').remove(), 2000);
 }
 
-// Event listener when clicking RETURN button
-cancelBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  table.style.display = 'block';
-  addBookDiv.style.display = 'block';
-  form.style.display = 'none';
-  form.reset();
-});
-
-// Event listener for delete item and toggle switch button
 list.addEventListener('click', (e) => {
   e.preventDefault();
 
-  if (e.target.classList.contains('delete')) {
+  // Remove book from list
+  if (
+    e.target.classList.contains('btn-delete') ||
+    e.target.classList.contains('fa-trash')
+  ) {
     // Show popup
-    swal('Are you sure you want to do this?', {
+    swal('Are you sure you want to delete the book?', {
       buttons: ['Cancel', 'Yes'],
     }).then((val) => {
       if (val) {
@@ -147,58 +192,56 @@ list.addEventListener('click', (e) => {
           text: 'You deleted the item',
           icon: 'success',
         });
-        e.target.parentElement.parentElement.remove();
+        let bookToRemove = e.target.parentElement.parentElement.parentElement;
+        bookToRemove.remove();
+
         // Remove book from Local Storage
         removeBookFromStorage(e.target);
+
+        // Hide table if there are no books
+        showHideTable();
       }
     });
   }
 
-  // Toggle button Yes/No for the read value
-  if (e.target.classList.contains('toggle')) {
+  // Change read status
+  if (e.target.classList.contains('toggle-status')) {
     valToUpdate = e.target.parentElement.previousElementSibling;
-    bookToUpdate =
-      e.target.parentElement.previousElementSibling.previousElementSibling
-        .previousElementSibling.previousElementSibling;
 
     if (valToUpdate.innerHTML == 'yes') {
       valToUpdate.innerHTML = 'no';
-      updateLocalStorage(bookToUpdate, valToUpdate);
+      updateLocalStorage(e.target, valToUpdate);
     } else {
       valToUpdate.innerHTML = 'yes';
-      updateLocalStorage(bookToUpdate, valToUpdate);
+      updateLocalStorage(e.target, valToUpdate);
     }
   }
 });
 
 // Remove book from Local Storage
 function removeBookFromStorage(book) {
-  // Get books from LS
   getLibrary();
-  // Get the title of the targeted book
-  let bookToRemove =
-    book.parentElement.previousElementSibling.previousElementSibling
-      .previousElementSibling.previousElementSibling.previousElementSibling
-      .innerHTML;
+
+  let bookToRemoveID = book.closest('.row').id;
 
   for (let i = 0; i < myLibrary.length; i++) {
-    if (myLibrary[i].title === bookToRemove) {
+    if (i == bookToRemoveID) {
       myLibrary.splice(i, 1);
     }
   }
+
   localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 }
 
-// Update read value from LS
+// Update read status in LS
 function updateLocalStorage(book, value) {
   getLibrary();
 
   for (let i in myLibrary) {
-    let val = myLibrary[i].title;
-    // If the book title is the same then set the read value of the same book equals to the value of the toggle
-    if (val == book.innerHTML) {
+    if (i === book.parentElement.parentElement.id) {
       myLibrary[i].read = value.innerHTML;
     }
   }
+
   localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 }
